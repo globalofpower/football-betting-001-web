00e3ef73ce11974a1a8a40ec7c9e143b163a266f
heinpyaephyomaw <heinpyaephyomaw@gmail.com>
2025-12-01 17:56:09 +0630

    complete

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -50,7 +50,7 @@ const Header = () => {
             className="sticky top-0 z-[99999] flex items-center bg-[var(--secondary-color)] shadow-[0 1px 2px 0 #3c40434d,0 1px 3px 1px #3c404326] w-full max-w-[500px] h-[55px] py-[0] px-[10px]"
         >
             <div className="w-full flex items-center justify-between">
-                <h1 className='text-white'>555 Mix</h1>
+                <h1 className='text-white'>234 Bet</h1>
                 <span className='text-[14px] text-[var(--white-color)] font-medium'>{footballTitleHandle()}</span>
                 <div className='flex items-center gap-1'>
                     <button
